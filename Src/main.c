8af011b906d0cf88ab0003c628b008d91ab3f4a2
Smushis <boyer.norman@gmail.com>
2019-10-30 16:31:21 +0100

    RTC

--- Src/main.c
+++ Src/main.c
@@ -5,6 +5,8 @@
 #include "stm32l4xx_ll_system.h"
 #include "stm32l4xx_ll_utils.h"
 #include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_rtc.h"
+#include "stm32l4xx_ll_pwr.h"
 #include "stm32l476xx.h"
 #include "core_cm4.h"
 #include "stm32l4xx_ll_cortex.h"
@@ -12,6 +14,9 @@
 // #if defined(USE_FULL_ASSERT)
 // #include "stm32_assert.h"
 // #endif /* USE_FULL_ASSERT */
+
+#define RTC_ASYNCH_PREDIV          ((uint32_t)0x7F)
+#define RTC_SYNCH_PREDIV           ((uint32_t)0x00FF)
 
 #include "gpio.h"
 
@@ -23,7 +28,8 @@ void     SystemClock_Config(void);
 
 
 int tick = 0;
-int expe = 2;
+int expe = 1;
+int blue_mode = 0;
 
 void SystemClock_Config2(void){
 	/* MSI configuration and activation */
@@ -57,16 +63,8 @@ void SystemClock_Config2(void){
 }
 
 void SysTick_Handler(void){
-	//LL_GPIO_TogglePin(LED_PORT, LED_PIN);
-	/*if(!stateLed){
-		LED_GREEN(1);
-		stateLed = 1;
-	}
-	else if(stateLed){
-		LED_GREEN(0);
-		stateLed = 0;
-	}*/
 	tick++;
+
 	if(tick%2 == 1){
 		LL_GPIO_SetOutputPin(PWM_PORT, PWM_PIN );
 	}
@@ -80,57 +78,109 @@ void SysTick_Handler(void){
 		LED_GREEN(1);
 		tick = 0;
 	}
-	/*if(expe == 1){
-		LL_LPM_EnableSleep();
-	}*/
 }
 
-int main(void)
+void MSICalibration_Process(void)
 {
+  /* LSE configuration and activation */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_PWR_EnableBkUpAccess();
+
+  if (LL_RCC_LSE_IsReady() == 0)
+  {
+      LL_RCC_ForceBackupDomainReset();
+      LL_RCC_ReleaseBackupDomainReset();
+      LL_RCC_LSE_Enable();
+  }
+
+  /* Enable MSI clock PLL */
+  LL_RCC_MSI_EnablePLLMode();
+
+  /* Calibration success*/
+  //LED_On();
+}
+
+void Configure_RTC(void){
+	  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+	  LL_PWR_EnableBkUpAccess();
+
+	  if (LL_RCC_LSE_IsReady() == 0)
+	  {
+	      LL_RCC_ForceBackupDomainReset();
+	      LL_RCC_ReleaseBackupDomainReset();
+	      LL_RCC_LSE_Enable();
+	  }
+	  LL_RCC_SetRTCClockSource(LL_RCC_RTC_CLKSOURCE_LSE);
+
+	  LL_RCC_EnableRTC();
+
+	  LL_RTC_DisableWriteProtection(RTC);
+	  LL_RTC_EnableInitMode(RTC);
+	  /*Truc à faire*/
+	  LL_RTC_SetHourFormat(RTC, LL_RTC_HOURFORMAT_AMPM);
+	  /* Set Asynch Prediv (value according to source clock) */
+	  LL_RTC_SetAsynchPrescaler(RTC, RTC_ASYNCH_PREDIV);
+	  /* Set Synch Prediv (value according to source clock) */
+	  LL_RTC_SetSynchPrescaler(RTC, RTC_SYNCH_PREDIV);
+	  LL_RTC_DisableInitMode(RTC);
+	  LL_RTC_EnableWriteProtection(RTC);
+}
+
+
+
+int main(void) {
+
 int stateButton = 0;
-expe = 2;
+int previousState = 0;
+
+// Config GPIO
+GPIO_init();
+
+//Initialisation module RTC
+Configure_RTC();
+
+
+// On récupère la variable expe
+// On l'incrémente si nécessaire
+expe = LL_RTC_BAK_GetRegister(RTC, LL_RTC_BKP_DR0);
+if (expe < 1) expe = 1;
+if(BLUE_BUTTON()){
+	expe++;
+	if (expe > 8) expe = 1;
+	LL_RTC_BAK_SetRegister(RTC, LL_RTC_BKP_DR0, expe);
+}
+
+
 /* Configure the system clock */
 if (expe == 2){
 	SystemClock_Config2();
 }
 else SystemClock_Config();
-// config GPIO
-GPIO_init();
 
-// init timer pour utiliser la fonction LL_mDelay() de stm32l4xx_ll_utils.c
-//LL_Init1msTick( SystemCoreClock );
-//SysTick->CTRL |= 1;
-//SysTick->CTRL |= 2;
-SysTick_Config(SystemCoreClock/100);
-
-//Activation du LSE pour future calibration
 if(expe == 2) {
-	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
-	LL_PWR_EnableBkUpAccess();
-	if(!LL_RCC_LSE_IsReady()){
-	    LL_RCC_ForceBackupDomainReset();
-	    LL_RCC_ReleaseBackupDomainReset();
-		LL_RCC_LSE_Enable();
-	}
-	LL_RCC_MSI_EnablePLLMode();
+	MSICalibration_Process();
 }
 
-
+// Configuration du SysTick
+// A faire une fois que les clocks sont ocnfigurées
+SysTick_Config(SystemCoreClock/100);
 
 while (1) {
-	if	( BLUE_BUTTON() ) {
+	if( BLUE_BUTTON() ) {
+		//On regarde si le bouton est pressé
+		previousState = stateButton;
 		stateButton = 1;
  	}
-	if(stateButton == 1){
-
-		//Activation de la calibration du MSI
+	//On détecte un nouvel appui sur le bouton
+	if(stateButton == 1 && previousState == 0){
+		blue_mode = 1;
+		/*
 		if(expe == 2) {
+				//Activation de la calibration du MSI
 			LL_RCC_MSI_EnablePLLMode();
 		}
-
-
 		LL_LPM_EnableSleep();
-		__WFI();
+		__WFI();*/
 	}
  }
 }
